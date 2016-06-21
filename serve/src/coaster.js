92d0bf5b557987850555edbe2abea7ec79b12dd9
MiLeung <michaeleleung@gmail.com>
2016-06-21 15:16:21 -0700

    vr

--- serve/src/coaster.js
+++ serve/src/coaster.js
@@ -8,6 +8,9 @@ render();
 function init() {
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
+  effect = new THREE.StereoEffect( renderer );
+  effect.setSize( window.innerWidth, window.innerHeight );
+  
   var element = renderer.domElement;
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -53,7 +56,8 @@ function render() {
   console.log('rendering');
 	requestAnimationFrame(render);
   controls.update();
-  renderer.render(scene, camera);
+//  renderer.render(scene, camera);
+  effect.render( scene, camera );
 };
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
